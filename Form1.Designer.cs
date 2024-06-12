9bf011205988de448051b35e89ab033a9641c2c1
david66boss <david.calbeaza@saguna.ro>
2024-06-12 09:20:01 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace SARPILIIIIII
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblScor;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblScor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // timer1
+            this.timer1.Interval = 100;
+
+            // lblScor
+            this.lblScor.AutoSize = true;
+            this.lblScor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScor.Location = new System.Drawing.Point(12, 9);
+            this.lblScor.Name = "lblScor";
+            this.lblScor.Size = new System.Drawing.Size(53, 17);
+            this.lblScor.TabIndex = 0;
+            this.lblScor.Text = "Scor: 0";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScor);
+            this.Name = "Form1";
+            this.Text = "Joc Snake";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SARPILIIIIII
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> sarpe = new List<Point>(); 
+        private Point mancare = Point.Empty;            
+        private int directie = 0;                     
+        private int scor = 0;                           
+        private Random random = new Random();           
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += new EventHandler(TimerTick);
+            this.KeyDown += new KeyEventHandler(FormKeyDown);
+            IncepeJocul();
+        }
+
+        private void IncepeJocul()
+        {
+            sarpe.Clear();
+            sarpe.Add(new Point(10, 10));
+            directie = 0;              
+            scor = 0;               
+            GenerareMancare();
+            timer1.Start();      
+        }
+
+        private void GenerareMancare()
+        {
+            int x = random.Next(0, this.ClientSize.Width / 10);
+            int y = random.Next(0, this.ClientSize.Height / 10);
+            mancare = new Point(x, y);
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            Point cap = sarpe[0];
+            Point nouCap = Point.Empty;
+
+            switch (directie)
+            {
+                case 0: nouCap = new Point(cap.X, cap.Y - 1); break;
+                case 1: nouCap = new Point(cap.X + 1, cap.Y); break;
+                case 2: nouCap = new Point(cap.X, cap.Y + 1); break;
+                case 3: nouCap = new Point(cap.X - 1, cap.Y); break;
+            }
+            sarpe.Insert(0, nouCap);
+
+            if (nouCap == mancare)
+            {
+                scor += 10;
+                GenerareMancare();
+            }
+            else
+            {
+                sarpe.RemoveAt(sarpe.Count - 1);
+            }
+
+            if (nouCap.X < 0 || nouCap.Y < 0 || nouCap.X >= this.ClientSize.Width / 10 || nouCap.Y >= this.ClientSize.Height / 10 || sarpe.GetRange(1, sarpe.Count - 1).Contains(nouCap))
+            {
+                timer1.Stop();
+                MessageBox.Show("Jocul s-a terminat! Scor: " + scor);
+                IncepeJocul();
+            }
+
+            this.Invalidate();
+        }
+
+        private void FormKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up: if (directie != 2) directie = 0; break;
+                case Keys.Right: if (directie != 3) directie = 1; break;
+                case Keys.Down: if (directie != 0) directie = 2; break;
+                case Keys.Left: if (directie != 1) directie = 3; break;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            foreach (Point p in sarpe)
+            {
+                e.Graphics.FillRectangle(Brushes.Green, new Rectangle(p.X * 10, p.Y * 10, 10, 10));
+            }
+
+            e.Graphics.FillRectangle(Brushes.Red, new Rectangle(mancare.X * 10, mancare.Y * 10, 10, 10));
+
+            lblScor.Text = "Scor: " + scor;
+        }
+    }
+}
